dd03a873fd53911ebf6b2145f003f23b9ea4484c
dmitriz <dmitri14@gmail.com>
2016-01-12 05:16:37 +0000

    Update Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,8 @@
+var os = require('os');
 var path = require('path');
 var gulp = require('gulp');
 
-var root = '/Users/dmitrizaitsev/';
+var root = os.homedir();
 
 var dotFiles = [
 	'.bash_profile',
@@ -16,6 +17,9 @@ gulp.task('default', ['collect']);
 
 // collect dot files 
 gulp.task('collect', function(){
-  return gulp.src(dotFiles.map(function(file){return root + file}))
+  return gulp.src(dotFiles.map(function(file){
+	  console.log("Copying", file);
+  	return path.resolve(root, file);
+  }))
   .pipe(gulp.dest('.'));
 });
